20cc4c6bfa1a2441e36401b5a7c4f39f990f6153
cacaopony <shikoku.fumiya@gmail.com>
2025-01-21 00:09:55 +0900

    first commit

---
+++ src/main/java/com/example/SharedFileEditServerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedFileEditServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedFileEditServerApplication.class, args);
+	}
+
+}
