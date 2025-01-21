1f4e702439746b0386fd65861208837267916984
F.Kudo <f.kudo@dynamic-plus.com>
2025-01-21 14:59:58 +0900

    develop

--- src/main/java/com/example/GetFileController.java
+++ src/main/java/com/example/GetFileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FileGetController {
+public class GetFileController {
 
     @GetMapping("/get-file")
     public ResponseEntity<String> getFileContent() {
